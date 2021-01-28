a92e371c6b1ac94ded1299d236248ae45474c342
Daniel Neo <78002139+danielneo27@users.noreply.github.com>
2021-01-28 11:00:33 -0300

    v0.2 Beta upd 3

--- Chocolatey-Scripts-Apps(PowerShell)/instalar-programas.ps1
+++ Chocolatey-Scripts-Apps(PowerShell)/instalar-programas.ps1
@@ -1,10 +1,13 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-$programas = "0"
+<#
 
-while ($programas -ne "s" -and $programas -ne "n") {
-    $programas = Read-Host "Instalar programas no sistema? (s/n) "
-    if ($programas -eq "s") {
-        foreach ($line in Get-Content $PSSCriptRoot\apps.txt) {
+    Script feito por Daniel Neo
+    Criado 22.01.2021
+    Versão  0.2 Beta
+    Atualizado 24.01.2021
+
+#>
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+foreach ($line in Get-Content $PSSCriptRoot\apps.txt) {
             if ($line.contains(":")) {
                 continue
             }
@@ -13,6 +16,4 @@ while ($programas -ne "s" -and $programas -ne "n") {
             cinst $line    
                
         }
-    }
-}
 choco feature disable -n allowGlobalConfirmation 
